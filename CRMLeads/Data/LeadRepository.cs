d966d2e862f1ac6ef8860618d31a98515f88c13f
Prajwal R Koushik <Prajwalk@talisma.com>
2024-04-29 13:31:45 +0530

    Add project files.

---
+++ CRMLeads/Data/LeadRepository.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.SqlClient;
+namespace CRMLeads.Data
+{
+    public class LeadRepository
+    {
+        private SqlConnection _connection;
+
+        public LeadRepository()
+        {
+            string connStr = "server=.; database=CRMLeads; " +
+                "Integrated Secutity=true; TrustServerCertificate=true";
+
+            _connection = new SqlConnection(connStr);
+
+        }
+
+        public LeadRepository(SqlConnection connection)
+        {
+            _connection = connection;
+
+        }
+
+         
+    }
+}
---
+++ CRMLeads/Models/LeadsEntity.cs
@@ -0,0 +1,16 @@
+﻿namespace CRMLeads.Models
+{
+    public class LeadsEntity
+    {
+        public int Id { get; set; }
+        public DateTime LeadDate { get; set; }
+        public string Name { get; set; }
+        public string EmailAddress { get; set; }
+        public string Mobile { get; set; }
+        public string LeadSource { get; set; }
+        public string LeadStatus { get; set; }
+        public DateTime NextFollowUpDate { get; set; }
+
+
+    }
+}

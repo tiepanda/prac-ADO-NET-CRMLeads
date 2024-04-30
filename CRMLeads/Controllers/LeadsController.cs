f3dcff005e5c73d69ee9eb6e295e0690424ed23f
Prajwal R Koushik <Prajwalk@talisma.com>
2024-04-30 09:58:35 +0530

    using ado.net  ,connected to database and now fetching data from data base

---
+++ CRMLeads/Controllers/LeadsController.cs
@@ -0,0 +1,19 @@
+﻿using CRMLeads.Data;
+using CRMLeads.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRMLeads.Controllers
+{
+    public class LeadsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<LeadsEntity> leads = new List<LeadsEntity>();
+            LeadRepository LeadRepository = new LeadRepository();
+
+            leads = LeadRepository.GetAllLeads();
+
+            return View(leads);
+        }
+    }
+}
--- CRMLeads/Data/LeadRepository.cs
+++ CRMLeads/Data/LeadRepository.cs
@@ -1,4 +1,6 @@
-﻿using System.Data.SqlClient;
+﻿using CRMLeads.Models;
+using System.Data;
+using System.Data.SqlClient;
 namespace CRMLeads.Data
 {
     public class LeadRepository
@@ -7,19 +9,48 @@ namespace CRMLeads.Data
 
         public LeadRepository()
         {
-            string connStr = "server=.; database=CRMLeads; " +
-                "Integrated Secutity=true; TrustServerCertificate=true";
+            string connStr = "server =.; database =CRMLeads; " +
+                "Integrated Security=True; TrustServerCertificate=true";
 
             _connection = new SqlConnection(connStr);
 
         }
 
-        public LeadRepository(SqlConnection connection)
+
+        public List<LeadsEntity> GetAllLeads()
         {
-            _connection = connection;
+            List<LeadsEntity> LeadsListEntity = new List<LeadsEntity>();
+
+            SqlCommand cmd = new SqlCommand("GetAllLeads" , _connection);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+
+            dataAdapter.Fill(dt);
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                LeadsListEntity.Add(
+                    new LeadsEntity
+                    {
+                        Id = Convert.ToInt32(dr["id"]),
+                        LeadDate = Convert.ToDateTime(dr["LeadDate"]),
+                        Name = dr["name"].ToString(),
+                        EmailAddress = dr["EmailAddress"].ToString(),
+                        Mobile = dr["Mobile"].ToString(),
+                        LeadSource = dr["LeadSource"].ToString(),
+                        LeadStatus = dr["LeadStatus"].ToString(),
+                        NextFollowUpDate = Convert.ToDateTime(dr["NextFollowUpDate"]),
+                    }
+                    );
+
+
+            }
+                
+            return LeadsListEntity;
 
         }
-
          
     }
 }
